05e87704fd5c48f6cee03c65f92921d42ec19106
Chris Jorenby <25773132+CJBravo1@users.noreply.github.com>
2020-02-11 12:37:55 -0600

    Updated current logged in user for auto login
    Added $USER in place for current logged in user

--- MArcade/InstallRetroPieUServer.sh
+++ MArcade/InstallRetroPieUServer.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 #Install RetroPie on Ubuntu Server
 
+user=whoami
+
 #Add 'arcade' user to sudoers (password required first time)
-sudo sed -i -e '$a\arcade ALL=(ALL) NOPASSWD:ALL' /etc/sudoers
+#sudo sed -i -e '$a\arcade ALL=(ALL) NOPASSWD:ALL' /etc/sudoers
+sudo echo "$USER ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers
 
 #Enable Universe repo, update/upgrade and install dependencies
 sudo apt-add-repository universe && sudo apt update && sudo apt upgrade -y && sudo apt install xorg openbox pulseaudio alsa-utils menu libglib2.0-bin python-xdg at-spi2-core libglib2.0-bin dbus-x11 git dialog unzip xmlstarlet vim nano --no-install-recommends -y
@@ -17,7 +20,8 @@ echo 'exec openbox-session' >> ~/.xsession
 echo 'if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then'  >> ~/.bash_profile && sed -i '$ a\  startx -- -nocursor >/dev/null 2>&1' ~/.bash_profile && sed -i '$ a\fi' ~/.bash_profile
 
 #Configure 'arcade' user to autologin
-sudo mkdir /etc/systemd/system/getty@tty1.service.d && sudo sh -c 'echo [Service] >> /etc/systemd/system/getty@tty1.service.d/override.conf' && sudo sed -i '$ a\ExecStart=' /etc/systemd/system/getty@tty1.service.d/override.conf && sudo sed -i '$ a\ExecStart=-/sbin/agetty --skip-login --noissue --autologin arcade %I $TERM' /etc/systemd/system/getty@tty1.service.d/override.conf && sudo sed -i '$ a\Type=idle' /etc/systemd/system/getty@tty1.service.d/override.conf
+sudo mkdir /etc/systemd/system/getty@tty1.service.d && sudo sh -c 'echo [Service] >> /etc/systemd/system/getty@tty1.service.d/override.conf' && sudo sed -i '$ a\ExecStart=' /etc/systemd/system/getty@tty1.service.d/override.conf
+sudo sed -i '$ a\ExecStart=-/sbin/agetty --skip-login --noissue --autologin $USER %I $TERM' /etc/systemd/system/getty@tty1.service.d/override.conf && sudo sed -i '$ a\Type=idle' /etc/systemd/system/getty@tty1.service.d/override.conf
 
 #Get Retropie Setup script and run it
 git clone --depth=1 https://github.com/RetroPie/RetroPie-Setup.git && sudo RetroPie-Setup/retropie_setup.sh
