d89da7b6e100d85a7ff375bdba6fcc721431e830
Chris Jorenby <25773132+CJBravo1@users.noreply.github.com>
2021-01-25 08:59:01 -0600

    Update AdvancedCopy.sh

--- AdvancedCopy.sh
+++ AdvancedCopy.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-
-#This Script is a copy of all commands from https://ostechnix.com/advanced-copy-add-progress-bar-to-cp-and-mv-commands-in-linux/
-#Written 12/04/2020
-
 #Get Core Utils
 wget http://ftp.gnu.org/gnu/coreutils/coreutils-8.32.tar.xz
 tar xvJf coreutils-8.32.tar.xz
 cd coreutils-8.32/
 
-#Get Advanced Copy 
+#Download Advanced Copy Patch
 wget https://raw.githubusercontent.com/jarun/advcpmv/master/advcpmv-0.8-8.32.patch
 
 #Apply Patch
@@ -18,11 +14,10 @@ patch -p1 -i advcpmv-0.8-8.32.patch
 ./configure
 make
 
-#Copy New Copy $Path
+#Copy New cp and mv commands
 sudo cp src/cp /usr/local/bin/cp
 sudo cp src/mv /usr/local/bin/mv
 
-#Auto Add Progress Bar to cp and mv commands
-echo 'alias cp='/usr/local/bin/cp -gR' ' >> ~/.bashrc
-echo 'alias mv='/usr/local/bin/mv -g' ' >> ~/.bashrc
-
+#Add Alias
+echo 'alias cp='/usr/local/bin/cp -gR'' > ~/.bashrc
+echo 'alias mv='/usr/local/bin/mv -g'' > ~/.bashrc
