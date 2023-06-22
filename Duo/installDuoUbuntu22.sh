1e3cb9748142f0fb224f1332b506c0c7bf9375ff
Chris Jorenby <25773132+CJBravo1@users.noreply.github.com>
2023-06-22 15:23:08 -0500

    Backup files before editing

--- Duo/installDuoUbuntu22.sh
+++ Duo/installDuoUbuntu22.sh
@@ -2,13 +2,13 @@
 
 # Check if running as root
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root."
+   echo -e "\e[31mThis script must be run as root.\e[0m"
    exit 1
 fi
 
 # Check if required arguments are provided
 if [[ $# -ne 3 ]]; then
-    echo "Usage: $0 <integration_key> <client_secret> <duo_hostname>"
+    echo -e "\e[31mUsage: $0 <integration_key> <client_secret> <duo_hostname>\e[0m"
     exit 1
 fi
 
@@ -20,7 +20,7 @@ duo_hostname="$3"
 # Define file paths
 sources_list_path="/etc/apt/sources.list.d/duosecurity.list"
 gpg_key_path="/etc/apt/trusted.gpg.d/duo.gpg"
-ssh_config_path="/etc/ssh/ssh_config"
+sshd_config_path="/etc/ssh/sshd_config"
 common_auth_path="/etc/pam.d/common-auth"
 
 # Define file content for sources.list.d
@@ -31,9 +31,9 @@ echo "$sources_list_content" | sudo tee "$sources_list_path" > /dev/null
 
 # Verify if the file was created successfully
 if [ -e "$sources_list_path" ]; then
-    echo "File created successfully at $sources_list_path"
+    echo -e "\e[32mFile created successfully at $sources_list_path\e[0m"
 else
-    echo "Failed to create the file at $sources_list_path"
+    echo -e "\e[31mFailed to create the file at $sources_list_path\e[0m"
     exit 1
 fi
 
@@ -42,9 +42,9 @@ sudo curl -s https://duo.com/DUO-GPG-PUBLIC-KEY.asc | sudo gpg --dearmor -o "$gp
 
 # Verify if the public key was imported successfully
 if [ -e "$gpg_key_path" ]; then
-    echo "Public key imported successfully at $gpg_key_path"
+    echo -e "\e[32mPublic key imported successfully at $gpg_key_path\e[0m"
 else
-    echo "Failed to import the public key at $gpg_key_path"
+    echo -e "\e[31mFailed to import the public key at $gpg_key_path\e[0m"
     exit 1
 fi
 
@@ -54,9 +54,16 @@ sudo apt update
 # Install Duo packages
 sudo apt install -y duo-unix
 
+# Backup files
+backup_suffix=".bak"
+
+sudo cp "$sources_list_path" "$sources_list_path$backup_suffix"
+sudo cp "$sshd_config_path" "$sshd_config_path$backup_suffix"
+sudo cp "$common_auth_path" "$common_auth_path$backup_suffix"
+
 # Configure Duo
 echo -e "auth    requisite    pam_unix.so nullok_secure\nauth    [success=1 default=ignore]    /lib64/security/pam_duo.so\nauth    requisite    pam_deny.so\nauth    required    pam_permit.so\nauth    optional    pam_cap.so" | sudo tee "$common_auth_path" > /dev/null
-echo "Modified $common_auth_path successfully"
+echo -e "\e[32mModified $common_auth_path successfully\e[0m"
 
 sudo tee /etc/duo/pam_duo.conf > /dev/null <<EOF
 [duo]
@@ -75,13 +82,13 @@ pushinfo = yes
 ; Automatically send a Duo Push when only one factor is required
 autopush = yes
 EOF
-echo "Modified /etc/duo/pam_duo.conf successfully"
+echo -e "\e[32mModified /etc/duo/pam_duo.conf successfully\e[0m"
 
-# Modify ssh_config
-sudo sed -i -e 's/^#UsePAM .*/UsePAM yes/' -e 's/^#UseDNS .*/UseDNS no/' -e '/^#UsePAM .*/a ChallengeResponseAuthentication yes' "$ssh_config_path"
-echo "Modified $ssh_config_path successfully"
+# Modify sshd_config
+sudo sed -i -e 's/^#UsePAM .*/UsePAM yes/' -e 's/^#UseDNS .*/UseDNS no/' -e '/^#UsePAM .*/a ChallengeResponseAuthentication yes' "$sshd_config_path"
+echo -e "\e[32mModified $sshd_config_path successfully\e[0m"
 
 # Restart SSH service
 sudo systemctl restart sshd
 
-echo "Duo has been installed and configured successfully."
+echo -e "\e[32mDuo has been installed and configured successfully.\e[0m"
