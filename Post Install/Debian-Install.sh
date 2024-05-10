622b7ed925811b9425f8644a391daaf59a1b4224
Chris Jorenby <25773132+CJBravo1@users.noreply.github.com>
2024-05-10 09:46:01 -0500

    Relocate Debian-Install.sh Script

--- Post Install/Debian-Install.sh
+++ Post Install/Debian-Install.sh
@@ -2,10 +2,13 @@
 # DebianInstall.sh
 # This script installs the necessary packages for a Debian system
 #Start with Updates
+echo "Running: sudo apt update"
 sudo apt update 
+echo "Running: sudo apt upgrade -y"
 sudo apt upgrade -y 
 
 #Install Standard Packages
+echo "Running: sudo apt install toilet fortune lolcat vim nano htop cockpit -y"
 sudo apt install toilet fortune lolcat vim nano htop cockpit -y
 
 #Add Bashrc Greeting
@@ -14,4 +17,5 @@ echo 'uptime -p | lolcat' >> ~/.bashrc
 echo 'fortune -s | lolcat' >> ~/.bashrc
 
 #Create SSH Keys
+echo "Running: ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa <<< y"
 ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa <<< y
