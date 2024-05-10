985543d1cb403f8bd301e486668d08660a1a6da6
Chris Jorenby <25773132+CJBravo1@users.noreply.github.com>
2024-05-10 09:30:15 -0500

    Create Debian-Install.sh
    Debian Based Post Install Script

---
+++ Debian-Install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# DebianInstall.sh
+# This script installs the necessary packages for a Debian system
+#Start with Updates
+sudo apt update 
+sudo apt upgrade -y 
+
+#Install Standard Packages
+sudo apt install toilet fortune lolcat vim nano htop cockpit -y
+
+#Add Bashrc Greeting
+echo 'echo "Welcome to $(hostname)" | toilet -f term -F border --gay' >> ~/.bashrc
+echo 'uptime -p | lolcat' >> ~/.bashrc
+echo 'fortune -s | lolcat' >> ~/.bashrc
