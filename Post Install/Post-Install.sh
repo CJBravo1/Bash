432c894f2fe843adfaafc4c365a86951ccf105fd
Chris Jorenby <25773132+CJBravo1@users.noreply.github.com>
2024-05-17 11:36:18 -0500

    Add Greeting to end of script and create ssh keys

--- Post Install/Post-Install.sh
+++ Post Install/Post-Install.sh
@@ -14,14 +14,6 @@ if [ "$(lsb_release -si)" = "Debian" ]; then
     echo "Running: sudo apt install toilet fortune lolcat vim nano htop cockpit -y"
     sudo apt install toilet fortune lolcat vim nano htop cockpit -y
 
-    # Add Bashrc Greeting
-    echo 'echo "Welcome to $(hostname)" | toilet -f term -F border --gay' >> ~/.bashrc
-    echo 'uptime -p | lolcat' >> ~/.bashrc
-    echo 'fortune -s | lolcat' >> ~/.bashrc
-
-    # Create SSH Keys
-    echo "Running: ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa <<< y"
-    ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa <<< y
 fi
 
 # Check if the OS is Fedora
@@ -38,9 +30,14 @@ if [ "$(lsb_release -si)" = "Fedora" ]; then
     # Install Standard Packages
     echo "Running: Standard Package Installs"
     sudo dnf install toilet fortune-mod lolcat vim nano htop -y
-
-    # Add Bashrc Greeting
-    echo 'echo "Welcome to $(hostname)" | toilet -f term -F border --gay' >> ~/.bashrc
-    echo 'uptime -p | lolcat' >> ~/.bashrc
-    echo 'fortune -s | lolcat' >> ~/.bashrc
 fi
+
+# Add Bashrc Greeting
+echo "Adding Greeting"
+echo 'echo "Welcome to $(hostname)" | toilet -f term -F border --gay' >> ~/.bashrc
+echo 'uptime -p | lolcat' >> ~/.bashrc
+echo 'fortune -s | lolcat' >> ~/.bashrc
+
+# Create SSH Keys
+echo "Creating SSH Keys"
+ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa <<< y
