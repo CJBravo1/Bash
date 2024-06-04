5aee50634fec41aaa46c6c0110b339c2a5310c11
Chris Jorenby <25773132+CJBravo1@users.noreply.github.com>
2024-06-04 16:05:29 -0500

    Update installPortainer.sh
    Add an echo to tell you where to go

--- Docker/installPortainer.sh
+++ Docker/installPortainer.sh
@@ -4,3 +4,5 @@ docker volume create portainer_data
 
 #Download and install the Portainer Server Container
 docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+
+echo "URL to Portainer: http://127.0.0.1:9090"
