6498de336d166cfdd8f4ffd32c02152cf0a800b7
Chris Jorenby <25773132+CJBravo1@users.noreply.github.com>
2024-06-19 10:38:40 -0500

    Check if the NFS mount is active

--- Docker/backupDocker.sh
+++ Docker/backupDocker.sh
@@ -4,22 +4,25 @@
 NFS_MOUNT="/mnt/backup"
 
 # Check if NFS mount exists
-if [ ! -d "$NFS_MOUNT" ]; then
+if [ -d "$NFS_MOUNT" ]; then
+    echo "NFS mount exists at $NFS_MOUNT"
+    # Create a backup directory with the current timestamp
+    BACKUP_DIR="${NFS_MOUNT}/$(date +%Y-%m-%d_%H-%M-%S)"
+    mkdir -p "${BACKUP_DIR}"
+
+    # Backup each running Docker container
+    for CONTAINER_ID in $(docker ps -q); do
+        CONTAINER_NAME=$(docker inspect -f '{{.Name}}' "${CONTAINER_ID}" | sed 's/\///')
+        BACKUP_FILE="${BACKUP_DIR}/${CONTAINER_NAME}.tar"
+
+        # Backup the container to a tar file
+        echo "Backing up ${CONTAINER_NAME} to ${BACKUP_FILE}"
+        docker export "${CONTAINER_ID}" > "${BACKUP_FILE}"
+    done
+
+    echo "Docker containers backup completed!"
+
+else
     echo "NFS mount does not exist at $NFS_MOUNT"
     exit 1
 fi
-
-# Create a backup directory with the current timestamp
-BACKUP_DIR="${NFS_MOUNT}/$(date +%Y-%m-%d_%H-%M-%S)"
-mkdir -p "${BACKUP_DIR}"
-
-# Backup each running Docker container
-for CONTAINER_ID in $(docker ps -q); do
-    CONTAINER_NAME=$(docker inspect -f '{{.Name}}' "${CONTAINER_ID}" | sed 's/\///')
-    BACKUP_FILE="${BACKUP_DIR}/${CONTAINER_NAME}.tar"
-
-    # Backup the container to a tar file
-    docker export "${CONTAINER_ID}" | pv -W > "${BACKUP_FILE}"
-done
-
-echo "Docker containers backup completed!"
