cb07965cfa85d44aca7b9679a1fa1c6999220612
Chris Jorenby <>
2024-06-18 22:54:27 -0500

    Start to add functions

--- Post Install/Post-Install.sh
+++ Post Install/Post-Install.sh
@@ -1,21 +1,64 @@
 #!/bin/bash
-#Run this after OS install. This script will install some standard packages and set up some basic configurations.
+# Run this after OS install. This script will install some standard packages and set up some basic configurations.
+
+# Functions
+# Check if gh is installed via dnf
+installGhCopilot() {
+    if command -v gh >/dev/null 2>&1; then
+        echo "gh is installed"
+        gh auth login
+        gh extension install github/gh-copilot
+        gh copilot alias bash >> ~/.bashrc
+    else
+        echo "gh is not installed"
+    fi
+}
+
+# Flatpacks
+installFlatpacks() {
+    flatpaks=(
+        app.drey.Damask
+        com.spotify.Client
+        com.transmissionbt.Transmission
+        org.raspberrypi.rpi-imager
+    )
+    for flatpak in "${flatpaks[@]}"; do
+        echo -e "\e[32mInstalling $flatpak\e[0m"  # Echo in green color
+        flatpak install -y $flatpak
+    done
+}
+
+# Google Chrome for Ubuntu
+installGoogleChrome() {
+    # Check if the OS is Ubuntu
+    if [ "$(lsb_release -si)" = "Ubuntu" ]; then
+        # Check if Google Chrome is installed
+        if ! dpkg -s google-chrome-stable >/dev/null 2>&1; then
+            # Install Google Chrome
+            echo -e "\e[32mInstalling Google Chrome\e[0m"  # Echo in green color
+            wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+            sudo dpkg -i google-chrome-stable_current_amd64.deb
+            sudo apt install -f -y
+            rm google-chrome-stable_current_amd64.deb
+        else
+            echo "Google Chrome is already installed"
+        fi
+    fi
+}
 
 # Check if the OS is Debian-based
-if [ -f /etc/os-release ] && grep -q -E '^(ID|ID_LIKE)="?(debian|ubuntu)"?' /etc/os-release; then
-    #Start with Updates
+if [ "$(lsb_release -si)" = "Debian" ] || [ "$(lsb_release -si)" = "Ubuntu" ]; then
+    # Start with Updates
     echo "Running: sudo apt update"
-    sudo apt update 
+    sudo apt update
     echo "Running: sudo apt upgrade -y"
-    sudo apt upgrade -y 
+    sudo apt upgrade -y
     # Install Standard Packages
-    echo "Running: sudo apt install toilet fortune lolcat vim nano htop cockpit -y"
-    sudo apt install toilet fortune lolcat vim nano htop cockpit -y
-
-fi
+    echo "Running: sudo apt install toilet fortune lolcat vim nano htop -y"
+    sudo apt install toilet fortune lolcat vim nano htop gh -y
 
 # Check if the OS is Fedora
-if [ -f /etc/os-release ] && grep -q -E '^(ID|ID_LIKE)="?(fedora)"?' /etc/os-release; then
+elif [ "$(lsb_release -si)" = "Fedora" ]; then
     # Set DNF Parallel Downloads
     echo "Running: sudo sed -i 's/^max_parallel_downloads=.*/max_parallel_downloads=10/' /etc/dnf/dnf.conf"
     sudo sed -i 's/^max_parallel_downloads=.*/max_parallel_downloads=10/' /etc/dnf/dnf.conf
@@ -24,35 +67,52 @@ if [ -f /etc/os-release ] && grep -q -E '^(ID|ID_LIKE)="?(fedora)"?' /etc/os-rel
     echo "Running: sudo sed -i 's/^#default_yes=default_no/default_yes=default_yes/' /etc/dnf/dnf.conf"
     sudo sed -i 's/^#default_yes=default_no/default_yes=default_yes/' /etc/dnf/dnf.conf
 
-    #Start with Updates
+    # Start with Updates
     echo "Running: sudo dnf makecache"
     sudo dnf makecache
     echo "Running: sudo dnf update -y"
     sudo dnf update -y
     # Install Standard Packages
     echo "Running: Standard Package Installs"
-    sudo dnf install toilet fortune-mod lolcat vim nano htop gh -y
-    
+    sudo dnf install toilet fortune-mod lolcat vim nano htop google-chrome-stable -y
+    sudo dnf remove firefox libreoffice -y
+
     # Check if a desktop environment is installed
     if [ -n "$XDG_CURRENT_DESKTOP" ]; then
-        echo "Desktop environment is installed"
-        #Install Extensions
-        sudo dnf install gnome-tweaks gnome-shell-extension-dash-to-dock gnome-shell-extension-appindicator -y
-        
-        #Install Applications
-        sudo dnf install google-chrome-stable dropbox -y
+        # Install Flatpacks
+        echo -e "\e[32mInstalling Flatpacks\e[0m"  # Echo in green color
+        installFlatpacks
 
-    else
-        echo "No desktop environment is installed"
+        # Install Google Chrome
+        echo -e "\e[32mInstalling Google Chrome\e[0m"  # Echo in green color
+        installGoogleChrome
     fi
 fi
 
 # Add Bashrc Greeting
-echo "Adding Greeting"
 echo 'echo "Welcome to $(hostname)" | toilet -f term -F border --gay' >> ~/.bashrc
 echo 'uptime -p | lolcat' >> ~/.bashrc
 echo 'fortune -s | lolcat' >> ~/.bashrc
 
+# Check if the user wants to install GitHub Copilot
+read -p "Do you want to install GitHub Copilot? (y/n): " install_copilot
+if [ "$install_copilot" = "y" ]; then
+    installGhCopilot
+fi
+
+# Check if a desktop environment is installed
+if [ -n "$XDG_CURRENT_DESKTOP" ]; then
+    # Install Flatpacks
+    echo -e "\e[32mInstalling Flatpacks\e[0m"  # Echo in green color
+    installFlatpacks
+
+    # Install Google Chrome
+    echo -e "\e[32mInstalling Google Chrome\e[0m"  # Echo in green color
+    installGoogleChrome
+fi
+
 # Create SSH Keys
 echo "Creating SSH Keys"
 ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa <<< y
+
+echo "End of Script"
