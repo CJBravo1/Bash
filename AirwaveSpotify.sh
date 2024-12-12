0eb6a28008eb934fcfd8104be9376c08b2e4cfd6
Chris Jorenby <25773132+CJBravo1@users.noreply.github.com>
2024-12-11 21:00:26 -0600

    AirwaveSpotify Script

---
+++ AirwaveSpotify.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo apt update && sudo apt install autoconf automake avahi-daemon build-essential git libasound2-dev libavahi-client-dev libconfig-dev libdaemon-dev libpopt-dev libssl-dev libtool xmltoman
+git clone https://github.com/mikebrady/shairport-sync.git
+cd shairport-sync
+autoreconf -i -f
+./configure --with-alsa --with-avahi --with-ssl=openssl --with-systemd --with-metadata
+make
+sudo make install
+
+cd
+
+sudo apt install -y apt-transport-https curl
+curl -sSL https://dtcooper.github.io/raspotify/key.asc | sudo apt-key add -v -
+echo 'deb https://dtcooper.github.io/raspotify raspotify main' | sudo tee /etc/apt/sources.list.d/raspotify.list
+sudo apt update
+sudo apt install raspotify
