05930e7d25e2c11cdb4f95289649562fbe7bad08
Chris Jorenby <25773132+CJBravo1@users.noreply.github.com>
2024-12-12 12:32:28 -0600

    Add Bluetooth Receiver Code

--- Raspberry Pi/AirwaveSpotify.sh
+++ Raspberry Pi/AirwaveSpotify.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+#Install Shairport (AirPlay)
 sudo apt update && sudo apt install autoconf automake avahi-daemon build-essential git libasound2-dev libavahi-client-dev libconfig-dev libdaemon-dev libpopt-dev libssl-dev libtool xmltoman
 git clone https://github.com/mikebrady/shairport-sync.git
 cd shairport-sync
@@ -9,8 +11,75 @@ sudo make install
 
 cd
 
+#Install Raspotify (Spotify Connect)
 sudo apt install -y apt-transport-https curl
 curl -sSL https://dtcooper.github.io/raspotify/key.asc | sudo apt-key add -v -
 echo 'deb https://dtcooper.github.io/raspotify raspotify main' | sudo tee /etc/apt/sources.list.d/raspotify.list
 sudo apt update
 sudo apt install raspotify
+
+cd
+
+#Install Bluetooth Audio ALSA Backend (bluez-alsa-utils)
+# Bluetooth Audio ALSA Backend (bluez-alsa-utils)
+    sudo apt update
+    sudo apt install -y --no-install-recommends bluez-tools bluez-alsa-utils
+
+    # Bluetooth settings
+    sudo tee /etc/bluetooth/main.conf >/dev/null <<'EOF'
+[General]
+Class = 0x200414
+DiscoverableTimeout = 0
+
+[Policy]
+AutoEnable=true
+EOF
+
+    # Bluetooth Agent
+    sudo tee /etc/systemd/system/bt-agent@.service >/dev/null <<'EOF'
+[Unit]
+Description=Bluetooth Agent
+Requires=bluetooth.service
+After=bluetooth.service
+
+[Service]
+ExecStartPre=/usr/bin/bluetoothctl discoverable on
+ExecStartPre=/bin/hciconfig %I piscan
+ExecStartPre=/bin/hciconfig %I sspmode 1
+ExecStart=/usr/bin/bt-agent --capability=NoInputNoOutput
+RestartSec=5
+Restart=always
+KillSignal=SIGUSR1
+
+[Install]
+WantedBy=multi-user.target
+EOF
+    sudo systemctl daemon-reload
+    sudo systemctl enable bt-agent@hci0.service
+
+    # Bluetooth udev script
+    sudo tee /usr/local/bin/bluetooth-udev >/dev/null <<'EOF'
+#!/bin/bash
+if [[ ! $NAME =~ ^\"([0-9A-F]{2}[:-]){5}([0-9A-F]{2})\"$ ]]; then exit 0; fi
+
+action=$(expr "$ACTION" : "\([a-zA-Z]\+\).*")
+
+if [ "$action" = "add" ]; then
+    bluetoothctl discoverable off
+    # disconnect wifi to prevent dropouts
+    #ifconfig wlan0 down &
+fi
+
+if [ "$action" = "remove" ]; then
+    # reenable wifi
+    #ifconfig wlan0 up &
+    bluetoothctl discoverable on
+fi
+EOF
+    sudo chmod 755 /usr/local/bin/bluetooth-udev
+
+    sudo tee /etc/udev/rules.d/99-bluetooth-udev.rules >/dev/null <<'EOF'
+SUBSYSTEM=="input", GROUP="input", MODE="0660"
+KERNEL=="input[0-9]*", RUN+="/usr/local/bin/bluetooth-udev"
+EOF
+}
