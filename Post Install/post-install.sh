6a1df058d7daee8532b78daa870307a24b5ffe08
Chris Jorenby <25773132+CJBravo1@users.noreply.github.com>
2025-06-06 16:01:34 -0500

    Correct dracut command

--- Post Install/post-install.sh
+++ Post Install/post-install.sh
@@ -388,9 +388,6 @@ fi
   echo -e "\e[32mEnrolling TPM2 unlock requires your existing LUKS2 unlock password\e[0m"
   sudo systemd-cryptenroll --tpm2-device=auto --tpm2-pcrs=7+14 "$CRYPT_DISK"
 # Modify /etc/crypttab to include tpm2-device=auto for the relevant LUKS device
-CRYPTTAB_FILE="/etc/crypttab"
-sudo cp  "$CRYPTTAB_FILE" "$CRYPTTAB_FILE.bak"
-LUKS_NAME=$(lsblk -no NAME,UUID | grep "$DISK_UUID" | awk '{print $1}')
 
 
 if [ -n "$LUKS_NAME" ]; then
@@ -401,7 +398,7 @@ else
 fi
 # Regenerate initramfs and update GRUB to accept the new enrollment
 if command -v dracut >/dev/null 2>&1; then
-    sudo dracut --force
+    sudo dracut --force --add tpm2-tss
     log_message "Regenerated initramfs with dracut."
 fi
 
